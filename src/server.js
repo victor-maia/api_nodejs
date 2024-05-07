7e7d296416710bf1ef479075056905664888d2e1
victor <victorluiscm@gmail.com>
2024-05-07 15:31:41 -0300

    added first route params

--- src/server.js
+++ src/server.js
@@ -4,8 +4,17 @@ const app = express();
 
 const PORT = 3333;
 
-app.get("/message", (request, response) => {
-    response.send("Hello World!")
+
+
+app.get("/message/:num/:user/:age", (request, response) => {
+
+    const {num, user, age} = request.params
+
+    response.send(`Num. da mensagem: ${num}
+        Nome do usuário: ${user}   
+        Idade do usuário: ${age}
+    `)
+
 });
 
 app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
