41556dabd66a267f7727ae129cac69d8fd7b3752
victor <victorluiscm@gmail.com>
2024-05-07 15:12:20 -0300

    added first get in node.js

--- src/server.js
+++ src/server.js
@@ -4,5 +4,9 @@ const app = express();
 
 const PORT = 3333;
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+app.get("/message", (request, response) => {
+    response.send("Hello World!")
+});
+
+app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
 
