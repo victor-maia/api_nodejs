24f4a4944d464e2dedd511c52e961f59294b42af
victor <victorluiscm@gmail.com>
2024-05-13 21:21:40 -0300

    organizing project directories responsibilities

--- src/server.js
+++ src/server.js
@@ -1,27 +1,11 @@
 const express = require("express");
-
 const app = express();
-
 const PORT = 3500;
+const routes = require("./routes")
 
+app.use(express.json())
+app.use(routes)
 
-
-app.get("/message/:num/:user/:age", (request, response) => {
-
-    const {num, user, age} = request.params
-
-    response.send(`Num. da mensagem: ${num}
-        Nome do usuário: ${user}   
-        Idade do usuário: ${age}
-    `)
-
-});
-
-app.get("/users", (request, response) => {
-    const {page, limit} = request.query
-
-    response.send(`Página, ${page}. Mostrar: ${limit}`)
-})
 
 app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
 
