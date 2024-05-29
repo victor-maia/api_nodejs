e4b4a763122809e6057000c47c94aada8b0703ff
Victor Maia <victorluiscm@gmail.com>
2024-05-29 17:43:40 -0300

    added functions in api to update informations of users

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,5 +1,6 @@
 const AppError = require('../utils/appError')
 const sqliteConnection = require('../database/sqlite')
+const { hash, compare} = require('bcryptjs');
 
 class UsersController {
   async create(request, response) {
@@ -12,8 +13,64 @@ class UsersController {
       throw new AppError("Este e-mail já está em uso!")
     }
 
+    const hashedPassword =  await hash(password, 8)
+
+    await database.run("INSERT INTO users (name, email, password) VALUES (?, ?, ?)", [name, email, hashedPassword])
+
     return response.status(201).json();
   }
+
+  async update(request, response) {
+    const { name, email, password, old_password} = request.body
+    const { id } = request.params
+
+    const database = await sqliteConnection()
+    const user = await database.get("SELECT * FROM users WHERE id = (?)", [id])
+
+    if(!user) {
+     throw new AppError("Usuário não encontrado")
+    }
+
+    const userWithUpdatedEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email])
+
+    if(userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
+     throw new AppError("Este e-mail já está em uso.")
+    }
+
+    user.name = name ?? user.name
+    user.email= email ?? user.email
+
+    if(password && !old_password){
+      throw new AppError("Você precisa informar a senha antiga para continuar.")
+    }
+
+    if(password && old_password){
+      const checkOldPassword = await compare(old_password, user.password)
+
+      if(!checkOldPassword){
+        throw new AppError("A Senha antiga não confere")
+      }
+
+      user.password = await hash(password, 8)
+
+    }
+
+    await database.run(`
+     UPDATE users SET
+     name = ?,
+     email = ?,
+     password = ?,
+     updated_at = DATETIME('now')
+     WHERE id = ?`, 
+     [user.name, user.email, user.password, id]
+   )
+
+   return response.json()
+ }
 }
+
+
+
+
 
 module.exports = UsersController
